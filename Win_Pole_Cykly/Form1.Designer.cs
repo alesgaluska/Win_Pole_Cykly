1dddd53e04b4e9c9737913e1bcbd95a835152cff
alesg <alesg@DESKTOP-HOUSE>
2017-02-10 22:34:34 +0100

    Add project files.

---
+++ Win_Pole_Cykly/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Win_Pole_Cykly
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_hraci = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_hraci
+            // 
+            this.txt_hraci.Location = new System.Drawing.Point(48, 51);
+            this.txt_hraci.Multiline = true;
+            this.txt_hraci.Name = "txt_hraci";
+            this.txt_hraci.Size = new System.Drawing.Size(468, 378);
+            this.txt_hraci.TabIndex = 0;
+            this.txt_hraci.TextChanged += new System.EventHandler(this.txt_hraci_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 467);
+            this.Controls.Add(this.txt_hraci);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_hraci;
+    }
+}
+
---
+++ Win_Pole_Cykly/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win_Pole_Cykly
+{
+    public partial class Form1 : Form
+    {
+        string[] pole_hraci = new string[25]; // musim defonovat velikost pole
+        List<string> list_hraci = new List<string>(); // nemusim definovat velikost pole !!!
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // klasicke pole kde musim defonovat velikost
+            pole_hraci[0] = "David";
+            pole_hraci[1] = "Lukas";
+            pole_hraci[2] = "Honza";
+
+            int i = 0;
+            while (i < 5)
+            {
+                txt_hraci.Text = txt_hraci.Text + i.ToString() + " " + pole_hraci[i] + Environment.NewLine;
+                i++;
+            }
+            txt_hraci.Text = txt_hraci.Text + Environment.NewLine + Environment.NewLine;
+
+            // novy typ pole kde nemusim definovat velikost a tedy obrovska vyhoda - list, vlozeni obsahu
+            list_hraci.Add("Fiko");
+            list_hraci.Add("Niko");
+            list_hraci.Add("Nic");
+            list_hraci.Add("HHHHHHHic");
+            list_hraci.Add("Kolic");
+
+            int x = 0;
+            while (x < list_hraci.Count)
+            {
+                txt_hraci.Text = txt_hraci.Text + x.ToString() + " " +list_hraci[x] + Environment.NewLine;
+                x++;
+            }
+            txt_hraci.Text = txt_hraci.Text + Environment.NewLine + Environment.NewLine;
+
+
+
+            // for cyklus
+            for (int y = 0; y < 100; y++)
+            {
+                txt_hraci.Text = txt_hraci.Text + y.ToString();
+            }
+
+
+        }
+
+        private void txt_hraci_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
